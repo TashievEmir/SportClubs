f27e9ac4cd28fd89640d598255e4016f80514d6f
Tashiev Emir <tashievemir01@gmail.com>
2024-01-24 22:19:53 +0600

    started adding announcement

---
+++ SportClubs/Controllers/AnnouncementController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportClubs.Models;
+
+namespace SportClubs.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    public class AnnouncementController : ControllerBase
+    {
+        public AnnouncementController ()
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok();
+        }
+    }
+}
